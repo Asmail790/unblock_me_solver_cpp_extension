aa034d942e79dde04f25f297ad4c52c849c2e929
Asmail <asmail790@gmail.com>
2023-10-13 09:06:12 +0200

    removed unnecessary decleration

--- src/includes/grid.hpp
+++ src/includes/grid.hpp
@@ -17,7 +17,6 @@ namespace UnblockMe::Utils::Grid {
 
     // implement std::equal_to and std::hash  for block to skip decltype(&hashOfBlock)?
     using Grid = std::set<Block>;
-    using ExperimentalGrid = std::unordered_set<const Block*,decltype(&hashOfBlock)>;
     auto getOccupiedPositions(const Grid& grid)->std::array<std::array<bool,6>,6>;
     auto getAvailablePositionsForBlock(const Block& block, const std::array<std::array<bool,6>,6>& occupied)->std::vector<std::pair<unsigned char,unsigned char>>;
     auto getAdjacentGrids(const Grid& grid)->std::set<Grid>;
